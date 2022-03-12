14c67050a802a628532d49f664baff002f33ae4f
Sandiewhyte <isianyasandra.is@gmail.com>
2022-03-12 22:26:04 +0100

    readme file

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print the alphabet in reverse order in lowercase. 
+ * Description: Print the alphabet in reverse order in lowercase.
  * Return: 0
  */
 int main(void)
