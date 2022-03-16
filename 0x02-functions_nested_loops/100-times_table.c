31d0c514b8d25039d9163146d0dd2decc1ca065a
Sandiewhyte <isianyasandra.is@gmail.com>
2022-03-16 01:50:52 +0100

    readme file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,10 +3,8 @@
 *print_times_table - Print the `n` times table, starting with 0.
 *Description: If `n` is greater than 15 or less than 0, print nothing.
 *@n: int type number
-Return: Always 0.
 */
-void print_times_table(int n)
-
+void print_times_table(int n);
 {
 	int x = 0, y, z;
 
@@ -46,7 +44,5 @@ void print_times_table(int n)
 	}
 	_putchar('\n');
 	x++;
-
 	}
-	return (0);
 }
